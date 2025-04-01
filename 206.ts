efa7d78d362382442f79878b613d51d71f36effb
Anubhav <anubhavgupta6205@gmail.com>
2025-04-01 11:35:20 +0530

    Leetcode-Easy: 206

--- 206.ts
+++ 206.ts
@@ -1,47 +1,29 @@
-let pivotIdx=Number.MIN_SAFE_INTEGER;
+/**
+ * Definition for singly-linked list.*/
 
-function search(nums: number[], target: number): number {
-     findPivot(0,nums.length-1,nums);
-     console.log(pivotIdx);
-     let pivotPoint = pivotIdx;
-    pivotIdx =Number.MIN_SAFE_INTEGER;
 
-    if(target>nums[pivotPoint] || pivotPoint === 0)
-    {
-        return binSearch(pivotPoint,nums.length-1,nums,target);
-    }
-    else
-    {
-        return binSearch(0,pivotPoint-1,nums,target);
-    }
-  
-};
-
-function binSearch(lb:number, ub:number, nums:number[], target: number)
-{
-    if(lb>up) return -1;
-    let mid = Math.floor((lb+ub)/2);
-    if(nums[mid] === target) return mid;
-    if(nums[mid]<target) return binSearch(mid+1,ub,nums,target);
-    return binSearch(lb,mid-1,nums,target);
-}
-
-function findPivot(lb:number,ub:number,nums:number[])
-{
-    if(ub-lb===1)
-    {
-        if(nums[ub]<nums[lb]){
-            pivotIdx = ub;
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+    public:
+        ListNode* reverseList(ListNode* head) { return reversedList(head); }
+    
+        ListNode* reversedList(ListNode* head) {
+            ListNode* prev = nullptr;
+            ListNode* nxt = nullptr;
+            ListNode* curr = head;
+            while (curr) {
+                nxt = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = nxt;
+            }
+            return prev;
         }
-        return;
-    }
-    let mid =Math.floor((ub+lb)/2);
-    if(nums[mid]>nums[ub])
-    {
-        findPivot(mid,ub,nums)
-    }
-    else if(nums[mid]<nums[lb])
-    {
-        findPivot(lb,mid,nums);
-    }    
-}
+    };
